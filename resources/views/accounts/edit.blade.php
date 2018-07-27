0733961aabc80ae86d68fa131e3ad9b576fa73aa
Damyan Pazvanski <damyan.pazvanski@mentormate.com>
2018-07-27 20:05:31 +0300

    .

--- resources/views/accounts/edit.blade.php
+++ resources/views/accounts/edit.blade.php
@@ -321,7 +321,7 @@
                         <label for="name" class="col-md-4 col-form-label text-right">Name</label>
 
                         <div class="col-md-6">
-                            
+
                         </div>
                     </div>
                 </div>
